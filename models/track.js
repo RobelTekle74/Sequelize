691cbf10fd788bae3d723a783f6cd1d070ef94e0
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2019-01-05 10:59:37 -0500

    more modificatins to scripts

--- models/track.js
+++ models/track.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
   const Track = sequelize.define('Track', {
     trackName: DataTypes.STRING,
     albumId: DataTypes.INTEGER,
-    trackDuration: DataTypes.INTEGER
+    trackDuration: DataTypes.STRING
   }, {});
   Track.associate = function(models) {
     // associations can be defined here
